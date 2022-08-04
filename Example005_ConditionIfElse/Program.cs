e317f75b88a61998942af69f143895e2d72acf46
GendrinVL <GendrinVL@gmail.com>
2022-08-04 19:04:34 +1000

    Excample 005 Condition If-Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите имя польхователя:");
+string? username = Console.ReadLine();
+if (username.ToLower() == "маша") Console.WriteLine("Ура это же МАША!");
+    else Console.WriteLine($"Привет:{username}");
